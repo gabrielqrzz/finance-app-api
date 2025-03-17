6574a625dac10a5ae6c402d134ae6dcfdcfbd989
Gabriel <gabrielqueiroz2510@gmail.com>
2025-03-17 19:26:20 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        //TODO: verificar se o email ja esta em uso
+
+        //gerar ID do usuario
+        const userId = uuidv4()
+
+        //criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+        //inserir o usuario no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        }
+
+        //chamar repositorio
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+        const createdUser = await postgresCreateUserRepository.execute(user)
+
+        return createdUser
+    }
+}
