ba78566817a7cb82652d859ff74a5378b172e3ea
Gabriel <gabrielqueiroz2510@gmail.com>
2025-03-20 19:47:29 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,45 @@
+import bcrypt from 'bcrypt'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        // se o email tiver sendo atualizado, verificar se ele ja esta em uso
+
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userWithProvidedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+        const user = {
+            ...updateUserParams,
+        }
+        // se a senha estiver sendo atualizada, cripografa-la
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+
+        //chamar o repository para atualizar o usuario
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const updatedUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updatedUser
+    }
+}
