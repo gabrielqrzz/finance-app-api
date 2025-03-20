8769b4f9c0185e80f1ad33ce53242a0284c15dde
Gabriel <gabrielqueiroz2510@gmail.com>
2025-03-19 22:23:00 -0300

    feat: ensure not found is returned if no user is found

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,5 +1,5 @@
 import { GetUserByIdUseCase } from '../use-cases/get-user-by-id.js'
-import { badRequest, ok, serverError } from './helpers.js'
+import { badRequest, notFound, ok, serverError } from './helpers.js'
 import validator from 'validator'
 
 export class GetUserByIdController {
@@ -9,7 +9,7 @@ export class GetUserByIdController {
 
             if (!isIdValid) {
                 return badRequest({
-                    message: 'The provided id is not valid',
+                    message: 'The provided id is not valid.',
                 })
             }
 
@@ -18,6 +18,12 @@ export class GetUserByIdController {
             const user = await getUserByIdUseCase.execute(
                 httpRequest.params.userId,
             )
+
+            if (!user) {
+                return notFound({
+                    message: 'User not found',
+                })
+            }
 
             return ok(user)
         } catch (error) {
