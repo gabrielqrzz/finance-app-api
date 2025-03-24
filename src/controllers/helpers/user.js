aed9c0fabfdbf65b0ccad2c6f7d011adc02af5b7
Gabriel <gabrielqueiroz2510@gmail.com>
2025-03-24 19:27:04 -0300

    feat: add helper functions to check if id is valid

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -19,3 +19,5 @@ export const invalidIdResponse = () =>
 export const checkIfPasswordIsValid = (password) => password.lenght >= 6
 
 export const checkIfEmailIsValid = (email) => validator.isEmail(email)
+
+export const checkIfIdIsValid = (id) => validator.isUUID(id)
