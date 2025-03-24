380816f36096daf0698aacb62a3152195ab9e374
Gabriel <gabrielqueiroz2510@gmail.com>
2025-03-24 19:22:19 -0300

    feat: add helper functions to user controllers

---
+++ src/controllers/helpers/user.js
@@ -0,0 +1,21 @@
+import validator from 'validator'
+import { badRequest } from '../helpers/http.js'
+
+export const invalidPasswordResponse = () =>
+    badRequest({
+        message: 'Password must be at least 6 characters',
+    })
+
+export const emailIsAlreadyInUseResponse = () =>
+    badRequest({
+        message: 'Invalid e-mail. Please provide a valid one.',
+    })
+
+export const invalidIdResponse = () =>
+    badRequest({
+        message: 'The provided ID is not valid',
+    })
+
+export const checkIfPasswordIsValid = (password) => password.lenght >= 6
+
+export const checkIfEmailIsValid = (email) => validator.isEmail(email)
