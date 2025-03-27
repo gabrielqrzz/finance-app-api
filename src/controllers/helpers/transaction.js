cdebbd797256a6bf36e038a535a4ee4d4ae2fcab
Gabriel <gabrielqueiroz2510@gmail.com>
2025-03-27 10:08:30 -0300

    fix: esnure that transaction amount is number and has 2 digital after decimals

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,22 +1,28 @@
 import validator from 'validator'
 import { badRequest } from './http.js'
 
-export const checkIfAmountIsValid = (amount) =>
-    validator.isCurrency(amount.toString(), {
+export const checkIfAmountIsValid = (amount) => {
+    if (typeof amount != 'number') {
+        return false
+    }
+
+    return validator.isCurrency(amount.toFixed(2), {
         digits_after_decimal: [2],
         allow_negatives: false,
         decimal_separator: '.',
     })
-
-export const checkIfTypeIsValid = (type) =>
-    ['EARNING', 'EXPENSE', 'INVESTIMENT'].includes(type)
-
-export const invalidAmountResponse = () =>
+}
+export const checkIfTypeIsValid = (type) => {
+    return ['EARNING', 'EXPENSE', 'INVESTIMENT'].includes(type)
+}
+export const invalidAmountResponse = () => {
     badRequest({
         message: 'The amount must be a valid currency.',
     })
+}
 
-export const invalidTypeResponse = () =>
+export const invalidTypeResponse = () => {
     badRequest({
         message: 'The type must be EARNING, EXPENSE or INVESTIMENT.',
     })
+}
