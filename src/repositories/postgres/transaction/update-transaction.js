12030de55752bd952043bc44a28513cd36c5d04c
Gabriel <gabrielqueiroz2510@gmail.com>
2025-03-27 10:17:47 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,29 @@
+import { PostgresHelpers } from '../../../db/postgres/helper.js'
+
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTransactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+        UPDATE transactions
+        SET ${updateFields.join(', ')}
+        WHERE id = $${updateValues.length}
+        RETURNING *
+        `
+
+        const updateUser = await PostgresHelpers.query(
+            updateQuery,
+            updateValues,
+        )
+
+        return updateUser[0]
+    }
+}
